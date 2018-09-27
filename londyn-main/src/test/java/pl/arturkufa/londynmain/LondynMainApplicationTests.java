336e652754c6db470ac2dc5306bf20db9a992d6d
Artur Laptop <sensibles93@gmail.com>
2018-09-27 12:30:05 +0200

    cross project dependencies, new unit tests runner

--- londyn-main/src/test/java/pl/arturkufa/londynmain/LondynMainApplicationTests.java
+++ londyn-main/src/test/java/pl/arturkufa/londynmain/LondynMainApplicationTests.java
@@ -3,11 +3,11 @@ package pl.arturkufa.londynmain;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class LondynMainApplicationTests {
 
 	@Ignore
